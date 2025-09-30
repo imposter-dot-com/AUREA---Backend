3cd59c6247c2990b0b9536763a75e7fdb39f6495
impostor-dot-com <nohurrying@gmail.com>
2025-09-30 08:47:15 +0700

    change cors

--- server.js
+++ server.js
@@ -24,8 +24,22 @@ const app = express();
 connectDB();
 
 // CORS configuration - Allow all origins
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://aurea-frontend.vercel.app/",
+  "https://localhost:3000"
+]
+
 const corsOptions = {
-  origin: true, // Allow all origins
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true); 
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
